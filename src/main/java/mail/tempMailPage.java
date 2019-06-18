01a393f3d25b1f1753c14a2ebcf299149f8d4ac3
ilyazlatkin <zlatkin_ilya@mail.ru>
2019-06-18 21:33:42 +0200

    Registaration Test Refactoring Mail services added

--- src/main/java/mail/tempMailPage.java
+++ src/main/java/mail/tempMailPage.java
@@ -3,20 +3,23 @@ package mail;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.HashSet;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
-public class tempMailPage {
+public class TempMailPage {
     private WebDriver driver;
 
     private By email = By.xpath("//input[@id='mail']");
     private By lastEmail = By.xpath("//div[@class=\\'inboxWarpMain\\']");
+    private By confirmation = By.xpath("//a[contains(text(),'Fintuity email confirm')]");
     private HashSet<WebElement> emails = new HashSet<WebElement>();
 
     private By refresh = By.id("click-to-refresh");
 
-    public tempMailPage(WebDriver driver){
+    public TempMailPage(WebDriver driver){
         this.driver = driver;
     }
 
@@ -24,30 +27,42 @@ public class tempMailPage {
         return driver.findElement(email).getAttribute("value");
     }
 
-    public tempMailPage refresh(){
+    public TempMailPage refresh(){
         driver.findElement(refresh).click();
-        return new tempMailPage(driver);
+        return new TempMailPage(driver);
     }
 
-    public tempMain_EmailBody clickLastEmail(){
+    public EmailBody clickLastEmail(){
         driver.findElement(lastEmail).click();
-        return new tempMain_EmailBody(this.driver);
+        return new EmailBody(this.driver);
     }
 
     public boolean isEmpty(){
         return driver.findElements(By.linkText("Your inbox is empty")).size() < 1;
     }
 
-    public boolean isReceived(int secondsToWait){
-        try {
-            Thread.sleep(secondsToWait * 1000);
-            refresh();
-            return driver.findElements(By.linkText("Your inbox is empty")).size() < 1;
-        } catch (InterruptedException e) {
-            return false;
-        }
+    public boolean isTextPresent(String text){
+        List<WebElement> list = driver.findElements(By.xpath("//*[contains(text(),'" + text + "')]"));
+        return list.size() > 0;
+    }
+
+    public void clickOnText(String text){
+        System.out.println("click on email with: " + text);
+        driver.findElement(By.xpath("//*[contains(text(),'" + text + "')]")).click();
+    }
+
+    public void clickConfirm(){
+        System.out.println("click on confirm email  " );
+        driver.findElement(confirmation).click();
+    }
+
+    public void waitForElement(String text){
+        WebDriverWait wait = new WebDriverWait(driver, 120);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//*[contains(text(),'" + text + "')]")));
 
     }
+
 
 
 
